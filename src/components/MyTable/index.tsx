28801a6185c1debbf1eee5ec112cfdf96a60605e
dengwenj <927561237@qq.com>
2022-10-01 18:18:27 +0800

    data-cell可以不传

--- src/components/MyTable/index.tsx
+++ src/components/MyTable/index.tsx
@@ -43,6 +43,48 @@ export default function MyTable() {
     }
   }, [])
 
+  // useEffect(() => {
+  //   // record {}
+  //   const res = Object.values({
+  //     type: '1080染缸',
+  //     jt: '1080A',
+  //     // info1: {
+  //     //   id: '1001',
+  //     //   yi: 'c:001152-0009',
+  //     //   er: '经销',
+  //     //   san: 'UQ-孟加拉工厂',
+  //     //   si: '33S/3_BCI棉/莫代尔 60/40',
+  //     //   wu: '悦达2261F03-A-2SW',
+  //     //   liu: '02403KKYL22',
+  //     //   qi: '31(30)浅米',
+  //     //   ba: 'C22099978T,C22090231',
+  //     //   jiu: '1128.12',
+  //     //   shi: '2022/09/21'
+  //     // },
+  //     info2: {
+  //       id: '1002',
+  //       yi: 'C:000550-0200-R1',
+  //       er: '经销',
+  //       san: 'UNIQLO-晶苑',
+  //       si: '32S/3_美棉_100+强捻+环锭',
+  //       wu: '尚和J32S3-07',
+  //       liu: '01787WHYL22',
+  //       qi: '01(01J)白色',
+  //       ba: 'C22090371',
+  //       jiu: '1186.00',
+  //       shi: '2022/09/23'
+  //     }
+  //   })
+    
+  //   const a: any[] = []
+  //   res.forEach((item) => {
+  //     if (typeof item !== 'string') {
+  //       a.push(item)
+  //     }
+  //   })
+  //   // a.find((item) => item.id === id)
+  // }, [])
+
   const render = (cell: any = {id: Math.random()}) => {
     // console.log(cell); 要想拿到新数据必须要发送请求更新表格
     
